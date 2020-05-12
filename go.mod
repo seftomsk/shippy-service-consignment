92fb5c4420999e207a9eb6fed290633bc348d67e
Alexey Verkhovtsev <seftomsk@gmail.com>
2020-05-12 18:57:00 +0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/seftomsk/shippy-service-consignment
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.1
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+	golang.org/x/sys v0.0.0-20200511232937-7e40ca221e25 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20200511104702-f5ebc3bea380 // indirect
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.22.0
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	pb "shippy-service-consignment/proto/consignment"
+	"sync"
+)
+
+const port = ":50051"
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type Repository struct {
+	mu sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+// Create a new consignment
+func (r *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	r.mu.Lock()
+	updated := append(r.consignments, consignment)
+	r.consignments = updated
+	r.mu.Unlock()
+	return consignment, nil
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(_ context.Context, consignment *pb.Consignment) (*pb.Response, error) {
+	// Save our consignment
+	consignment, err := s.repo.Create(consignment)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	// Set-up our gRPC server.
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	server := grpc.NewServer()
+
+	// Register our service with the gRPC server, this will tie our
+	// implementation into the auto-generated interface code for our
+	// protobuf definition.
+	pb.RegisterShippingServiceServer(server, &service{repo})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(server)
+
+	log.Println("Running on port: ", port)
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
